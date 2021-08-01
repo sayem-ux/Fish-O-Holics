77a5eedbd6fc1660340e1e5f3ad4f76e53300647
sayem-ux <sayem7333@gmail.com>
2021-08-01 17:22:04 -0400

    Added spacing between tables in serivces' page

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 function myFunction() 
 {
     const name = document.getElementsByTagName("input");
@@ -6,3 +5,5 @@ function myFunction()
     document.getElementById("result").innerHTML= nameVal + ", you will be notified in 7-12 business days.";
     
 } 
+
+var items = document.
