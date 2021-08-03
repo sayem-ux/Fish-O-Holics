17f96cb99a3eb9adcff487330687dfef9e6b1631
sayem <sayem7333@gmail.com>
2021-08-03 02:41:48 -0400

    added hover feature for quote and tips on services page / added legend for sales

--- main.js
+++ main.js
@@ -2,8 +2,36 @@ function myFunction()
 {
     const name = document.getElementsByTagName("input");
     const nameVal = name[2].value;
+    
+    if (nameVal != "") 
     document.getElementById("result").innerHTML= nameVal + ", you will be notified in 7-12 business days.";
     
 } 
 
-var items = document.
+// function for generating random numbers
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+var sales = document.getElementsByClassName("item-sale");
+
+for(let i=0; i<getRandomInt(20); i++)
+{
+    sales[getRandomInt(sales.length)].style.backgroundColor = "yellow";
+}
+
+var quoteBorder = document.getElementsByClassName("dark");
+quoteBorder[0].addEventListener("mouseenter", ON);
+quoteBorder[0].addEventListener("mouseleave", OFF);
+
+function ON(e) {quoteBorder[0].style.border="#e8491d solid 6px";}
+function OFF(e) {quoteBorder[0].style.border="";}
+
+var tipsBorder = document.querySelector(".light");
+tipsBorder.addEventListener("mouseenter", () => 
+    {tipsBorder.style.border= "#35424a solid 6px"});
+tipsBorder.addEventListener("mouseleave", () => 
+    {tipsBorder.style.border= ""});
+
+
+
