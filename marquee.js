3204c7399d9a9a4f5f8444cfa8571189d7f123de
sayem <sayem7333@gmail.com>
2021-09-14 01:19:25 -0400

    Added Un-Pause feature for the marquee effect

--- marquee.js
+++ marquee.js
@@ -1,5 +1,13 @@
+// NOTE:
+// The first pause is used for the marquee effect that starts with the page
+// The second pause is used when the marquee effect was manually re-started after the first pause. This causes a loop :)
+// The Un-Pause is used for both first and second pause.
 
+ 
 // Create a marquee effect : DONE
+
+const para1 = document.getElementById("para1");
+animate(para1);
 
 function animate(element) {
     let elementWidth = element.offsetWidth;
@@ -14,15 +22,30 @@ function animate(element) {
             }
         }, 7);
 
-// Pause marque effect when hovering over text : DONE
-// Un-Pause marque effect when hovering over text : DONE
-    element.addEventListener("mouseover", function(e) {
+    // Pause marque effect when hovering over text : DONE
+    element.parentElement.addEventListener("mouseover", function(e) {
         clearInterval(intervalID);
     })
+
+    // Un-Pause marque effect when hovering over away from text : DONE
+    element.parentElement.addEventListener("mouseout", function() {
+        var intervalID2 = setInterval(() => {
+             element.style.marginLeft = --flag + "px";
+     
+             if (elementWidth == -flag) {
+                 flag = parentWidth;
+             }
+         }, 7);
+
+        // Pause(2) marque effect when hovering over text : DONE
+        element.parentElement.addEventListener("mouseover", function() {
+        clearInterval(intervalID2);
+    })
+     })
 }
 
 
 
-const para1 = document.getElementById("para1");
-animate(para1);
+
+
 
