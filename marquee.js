76ded0ec1b583b7cfc28aabccd1b68ac01e39658
sayem-ux <sayem7333@gmail.com>
2021-09-13 23:19:52 -0400

    Added pause effect for marquee

--- marquee.js
+++ marquee.js
@@ -1,20 +1,28 @@
 
-// Create a marquee effect
-
-const para1 = document.getElementById("para1");
+// Create a marquee effect : DONE
 
 function animate(element) {
     let elementWidth = element.offsetWidth;
     let parentWidth = element.parentElement.offsetWidth;
     let flag = 0;
 
-    setInterval(() => {
-        element.style.marginLeft = --flag + "px";
+    var intervalID = setInterval(() => {
+            element.style.marginLeft = --flag + "px";
+    
+            if (elementWidth == -flag) {
+                flag = parentWidth;
+            }
+        }, 7);
 
-        if (elementWidth == -flag) {
-            flag = parentWidth;
-        }
-    }, 7);
+// Pause marque effect when hovering over text : DONE
+// Un-Pause marque effect when hovering over text : DONE
+    element.addEventListener("mouseover", function(e) {
+        clearInterval(intervalID);
+    })
 }
 
+
+
+const para1 = document.getElementById("para1");
 animate(para1);
+
